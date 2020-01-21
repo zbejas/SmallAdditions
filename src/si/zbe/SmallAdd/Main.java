aee5b43c310d829774fd66187afded1995e72903
Zbe <mavric.insane@gmail.com>
2020-01-21 16:08:03 +0100

    Update Main.java

--- src/si/zbe/SmallAdd/Main.java
+++ src/si/zbe/SmallAdd/Main.java
@@ -7,7 +7,9 @@ import org.bukkit.event.Listener;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import me.bramhaag.example.Updater;
 import si.zbe.Commands.AutoFeedCommand;
+import si.zbe.Commands.UpdateCommand;
 import si.zbe.Commands.WorkbenchCommand;
 import si.zbe.Events.CropEvent;
 import si.zbe.Events.FoodEvent;
@@ -21,6 +23,7 @@ public class Main extends JavaPlugin {
 
 	public void onEnable() {
 		plugin = this;
+		updateCheck();
 		setConfig();
 		registerCommands();
 		registerEvents();
@@ -39,13 +42,16 @@ public class Main extends JavaPlugin {
 		} else {
 			getLogger().info(Messages.getString("SA.AutoFeedDisabled"));
 		}
-		
+
 		// WORKBENCH
 		if (this.config.getBoolean("Workbench")) {
 			getCommand("portableworkbench").setExecutor((CommandExecutor) new WorkbenchCommand(this));
 		} else {
 			getLogger().info(Messages.getString("SA.WorkbenchDisabled"));
 		}
+		
+		//UPDATER
+		getCommand("saupdate").setExecutor((CommandExecutor) new UpdateCommand(this));
 	}
 
 	public void registerEvents() {
@@ -55,7 +61,7 @@ public class Main extends JavaPlugin {
 		} else {
 			getLogger().info(Messages.getString("SA.CropsDisabled"));
 		}
-		
+
 		// AUTOFEED
 		if (this.config.getBoolean("AutoFeed")) {
 			getServer().getPluginManager().registerEvents((Listener) new FoodEvent(), (Plugin) this);
@@ -63,14 +69,14 @@ public class Main extends JavaPlugin {
 		} else {
 			getLogger().info(Messages.getString("SA.AutoFeedDisabled"));
 		}
-		
+
 		// NOTRAMPLE
 		if (this.config.getBoolean("NoTrample")) {
 			getServer().getPluginManager().registerEvents((Listener) new TrampleEvent(), (Plugin) this);
 		} else {
 			getLogger().info(Messages.getString("SA.NoTrampleDisabled"));
 		}
-		
+
 		// WORKBENCH
 		if (this.config.getBoolean("Workbench")) {
 			getServer().getPluginManager().registerEvents((Listener) new WorkbenchEvent(), (Plugin) this);
@@ -89,5 +95,19 @@ public class Main extends JavaPlugin {
 		saveConfig();
 		this.config.options().header(Messages.getString("SA.ConfigLanguage"));
 		saveConfig();
+	}
+
+	public void updateCheck() {
+		Updater updater = new Updater(this, "74452");
+		Updater.UpdateResults result = updater.checkForUpdates();
+		getLogger().info(Messages.getString("SA.UpdateCheck"));
+		if (result.getResult() == Updater.UpdateResult.FAIL) {
+			getLogger().info(Messages.getString("SA.UpdateFail") + result.getVersion());
+		} else if (result.getResult() == Updater.UpdateResult.NO_UPDATE) {
+			getLogger().info(Messages.getString("SA.NoUpdate"));
+
+		} else if (result.getResult() == Updater.UpdateResult.UPDATE_AVAILABLE) {
+			getLogger().info(Messages.getString("SA.UpdateFound"));
+		}
 	}
 }
