529c4c140422cae912f6b01ae0ff3bf56a4509c2
Zbe <mavric.insane@gmail.com>
2020-02-02 18:14:27 +0100

    Update TitleManager.java

--- src/main/java/si/zbe/smalladd/utils/TitleManager.java
+++ src/main/java/si/zbe/smalladd/utils/TitleManager.java
@@ -9,7 +9,7 @@ public class TitleManager {
 
     /*
     *   This will probably break every few updates
-    *   Most of these haven't even been tested
+    *   Lol titles are now in spigot, nevermind
      */
 
     public void sendTitleActionBar(Player player, String message) {
@@ -20,39 +20,5 @@ public class TitleManager {
     public void sendTitleActionBar(Player player, String message, int fadein, int duration, int fadeout) {
         PacketPlayOutTitle title = new PacketPlayOutTitle(PacketPlayOutTitle.EnumTitleAction.ACTIONBAR, IChatBaseComponent.ChatSerializer.a("{\"text\":\" " + message + "\"}"), fadein, duration, fadeout);
         ((CraftPlayer) player).getHandle().playerConnection.sendPacket(title);
-    }
-
-    public void sendTitle(Player player, String title) {
-        PacketPlayOutTitle ptitle = new PacketPlayOutTitle(PacketPlayOutTitle.EnumTitleAction.TITLE, IChatBaseComponent.ChatSerializer.a("{\"text\":\" " + title + "\"}"), 25, 50, 25);
-        ((CraftPlayer) player).getHandle().playerConnection.sendPacket(ptitle);
-    }
-
-    public void sendTitle(Player player, String title, int fadein, int duration, int fadeout) {
-        PacketPlayOutTitle ptitle = new PacketPlayOutTitle(PacketPlayOutTitle.EnumTitleAction.TITLE, IChatBaseComponent.ChatSerializer.a("{\"text\":\" " + title + "\"}"), fadein, duration, fadeout);
-        ((CraftPlayer) player).getHandle().playerConnection.sendPacket(ptitle);
-    }
-
-    public void sendSubtitle(Player player, String subtitle) {
-        PacketPlayOutTitle ptitle = new PacketPlayOutTitle(PacketPlayOutTitle.EnumTitleAction.SUBTITLE, IChatBaseComponent.ChatSerializer.a("{\"text\":\" " + subtitle + "\"}"), 25, 50, 25);
-        ((CraftPlayer) player).getHandle().playerConnection.sendPacket(ptitle);
-    }
-
-    public void sendSubtitle(Player player, String subtitle, int fadein, int duration, int fadeout) {
-        PacketPlayOutTitle ptitle = new PacketPlayOutTitle(PacketPlayOutTitle.EnumTitleAction.SUBTITLE, IChatBaseComponent.ChatSerializer.a("{\"text\":\" " + subtitle + "\"}"), fadein, duration, fadeout);
-        ((CraftPlayer) player).getHandle().playerConnection.sendPacket(ptitle);
-    }
-
-    public void sendTitleAndSubtitle(Player player, String title, String subtitle) {
-        PacketPlayOutTitle ptitle = new PacketPlayOutTitle(PacketPlayOutTitle.EnumTitleAction.TITLE, IChatBaseComponent.ChatSerializer.a("{\"text\":\" " + title + "\"}"), 25, 50, 25);
-        PacketPlayOutTitle psubtitle = new PacketPlayOutTitle(PacketPlayOutTitle.EnumTitleAction.SUBTITLE, IChatBaseComponent.ChatSerializer.a("{\"text\":\" " + subtitle + "\"}"), 25, 50, 25);
-        ((CraftPlayer) player).getHandle().playerConnection.sendPacket(ptitle);
-        ((CraftPlayer) player).getHandle().playerConnection.sendPacket(psubtitle);
-    }
-
-    public void sendTitleAndSubtitle(Player player, String title, String subtitle, int fadein, int duration, int fadeout) {
-        PacketPlayOutTitle ptitle = new PacketPlayOutTitle(PacketPlayOutTitle.EnumTitleAction.TITLE, IChatBaseComponent.ChatSerializer.a("{\"text\":\" " + title + "\"}"), fadein, duration, fadeout);
-        PacketPlayOutTitle psubtitle = new PacketPlayOutTitle(PacketPlayOutTitle.EnumTitleAction.SUBTITLE, IChatBaseComponent.ChatSerializer.a("{\"text\":\" " + subtitle + "\"}"), fadein, duration, fadeout);
-        ((CraftPlayer) player).getHandle().playerConnection.sendPacket(ptitle);
-        ((CraftPlayer) player).getHandle().playerConnection.sendPacket(psubtitle);
     }
 }
