2d25caa8014afe9914207af0d7ea743667c0a5c0
Zbe <mavric.insane@gmail.com>
2020-02-02 16:21:42 +0100

    Update SpawnerEvent.java

--- src/main/java/si/zbe/smalladd/events/SpawnerEvent.java
+++ src/main/java/si/zbe/smalladd/events/SpawnerEvent.java
@@ -1,21 +1,25 @@
 package si.zbe.smalladd.events;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.CreatureSpawner;
 import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.BlockStateMeta;
+import si.zbe.smalladd.Main;
+import si.zbe.smalladd.Messages;
 
 public class SpawnerEvent implements Listener {
 	@EventHandler
-	public void onSpwnerBreak(BlockBreakEvent e) {
+	public void onSpawnerBreak(BlockBreakEvent e) {
 		Block block = e.getBlock();
 		if (block.getType() != Material.SPAWNER)
 			return;
@@ -32,8 +36,22 @@ public class SpawnerEvent implements Listener {
 		BlockStateMeta meta = (BlockStateMeta) item.getItemMeta();
 		CreatureSpawner newspawner = (CreatureSpawner) meta.getBlockState();
 
+		List<String> bannedspawners = Main.plugin.getConfig().getStringList("BannedSpawners");
+
+		for (String s : bannedspawners) {
+			try {
+				EntityType spawner = EntityType.valueOf(s.toUpperCase());
+				if (brokenspawner.getSpawnedType().equals(spawner)) {
+					e.getPlayer().sendMessage(ChatColor.RED + Messages.getString("SA.SpawnerTypeDisabled"));
+					return;
+				}
+			} catch (Exception ex) {
+				return;
+			}
+		}
+
 		ArrayList<String> lore = new ArrayList<String>();
-		lore.add(ChatColor.GOLD + "Spawner type: " + ChatColor.RED + brokenspawner.getSpawnedType());
+		lore.add(ChatColor.GOLD + Messages.getString("SA.SpawnerType") + ChatColor.RED + brokenspawner.getSpawnedType());
 
 		newspawner.setSpawnedType(brokenspawner.getSpawnedType());
 		meta.setBlockState(newspawner);
