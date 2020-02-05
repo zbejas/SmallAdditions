42b5e84b6f0f1c49cb238fa95075db9c9f8c610e
Zbe <mavric.insane@gmail.com>
2020-02-05 02:23:09 +0100

    Update UpdateEvent.java

--- src/main/java/si/zbe/smalladd/events/UpdateEvent.java
+++ src/main/java/si/zbe/smalladd/events/UpdateEvent.java
@@ -4,29 +4,26 @@ import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.plugin.Plugin;
-
 import si.zbe.smalladd.Main;
 import si.zbe.smalladd.Messages;
 import si.zbe.smalladd.UpdateChecker;
 
 public class UpdateEvent implements Listener {
-	@EventHandler
-	public void onJoin(PlayerJoinEvent e) {
-		if (!e.getPlayer().hasPermission("smalladd.admin"))
-			return;
+    @EventHandler
+    public void onJoin(PlayerJoinEvent e) {
+        if (!e.getPlayer().hasPermission("smalladd.admin"))
+            return;
 
-		if (!Main.plugin.getConfig().getBoolean("UpdateNotification"))
-			return;
+        if (!Main.plugin.getConfig().getBoolean("UpdateNotification"))
+            return;
 
-		(new UpdateChecker(Main.plugin, 74452)).getVersion(version -> {
-			if (!Main.plugin.getDescription().getVersion().equalsIgnoreCase(version)) {
-				e.getPlayer().sendMessage(ChatColor.GREEN + "[SmallAdditions] " + Messages.getString("SA.UpdateFound"));
-				e.getPlayer().sendMessage(ChatColor.GREEN + "                 " + ChatColor.RED + Main.plugin.getDescription().getVersion() + ChatColor.WHITE
-						+ " -> " + ChatColor.GREEN + version);
-				e.getPlayer().sendMessage(ChatColor.AQUA + "https://www.spigotmc.org/resources/smalladditions.74452/");
-			} else
-				return;
-		});
-	}
+        (new UpdateChecker(Main.plugin, 74452)).getVersion(version -> {
+            if (!Main.plugin.getDescription().getVersion().equalsIgnoreCase(version)) {
+                e.getPlayer().sendMessage(ChatColor.GREEN + "[SmallAdditions] " + Messages.getString("SA.UpdateFound"));
+                e.getPlayer().sendMessage(ChatColor.GREEN + "                 " + ChatColor.RED + Main.plugin.getDescription().getVersion() + ChatColor.WHITE
+                        + " -> " + ChatColor.GREEN + version);
+                e.getPlayer().sendMessage(ChatColor.AQUA + "https://www.spigotmc.org/resources/smalladditions.74452/");
+            }
+        });
+    }
 }
