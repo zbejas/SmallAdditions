2049c3bde39e5b1e759a7d8080d4a262cdbceabe
Zbe <mavric.insane@gmail.com>
2020-02-10 14:15:25 +0100

    Create FullInvEvent.java

---
+++ src/main/java/si/zbe/smalladd/events/FullInvEvent.java
@@ -0,0 +1,21 @@
+package si.zbe.smalladd.events;
+
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.entity.EntityPickupItemEvent;
+import si.zbe.smalladd.utils.TitleManager;
+
+public class FullInvEvent implements Listener {
+    @EventHandler
+    public void onPickUp(BlockBreakEvent e) {
+        Player p = (Player) e.getPlayer();
+
+        if (p.getInventory().firstEmpty() == -1) {
+            TitleManager tm = new TitleManager();
+            tm.sendTitleActionBar(p, ChatColor.RED + "Your inventory is full!", 50, 100, 50);
+        }
+    }
+}
