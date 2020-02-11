b0dea52b79c412e598987ca83a8a4ee4dbc48ec5
Zbe <mavric.insane@gmail.com>
2020-02-11 21:25:52 +0100

    Update PlayerDeadEvent.java

--- src/main/java/si/zbe/smalladd/events/PlayerDeadEvent.java
+++ src/main/java/si/zbe/smalladd/events/PlayerDeadEvent.java
@@ -55,7 +55,7 @@ public class PlayerDeadEvent implements Listener {
         DeathManager dm = new DeathManager();
         String deaths = "\n\n";
 
-        ArrayList<Location> list = dm.getDeaths(e.getPlayer());
+        ArrayList <Location> list = dm.getDeaths(e.getPlayer());
 
         Collections.reverse(list);
 
@@ -75,7 +75,7 @@ public class PlayerDeadEvent implements Listener {
             }
         }
 
-        BaseComponent[] lastdeath = new ComponentBuilder( ChatColor.BLACK + "" + ChatColor.BOLD + "     Last death    ")
+        BaseComponent[] lastdeath = new ComponentBuilder(ChatColor.BLACK + "" + ChatColor.BOLD + "     Last death    ")
                 .event(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/sad teleport"))
                 .event(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder(ChatColor.GOLD + "Teleport to last death location.").create()))
                 .append(deaths, ComponentBuilder.FormatRetention.NONE)
